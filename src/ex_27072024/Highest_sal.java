113635d60f53b6bfe2e9c3568e315fe52e417954
mandal-atanu <atanu1997.mandal@gmail.com>
2024-08-04 18:00:49 +0530

    27th July 2024.........

---
+++ src/ex_27072024/Highest_sal.java
@@ -0,0 +1,31 @@
+package ex_27072024;
+
+import java.util.Scanner;
+
+public class Highest_sal {
+    public static void main(String[] args) {
+
+        int salary[] = new int[4];
+
+        Scanner src = new Scanner(System.in);
+
+        for (int i = 0; i < salary.length ; i++) {
+            System.out.println("Enter the salary of person"+ (i+1));
+            salary[i] = src.nextInt();
+
+        }
+
+        int highest_sal = salary[0];
+
+        for (int i = 0; i < salary.length; i++) {
+            if (salary[i] > highest_sal){
+                highest_sal = salary[i];
+            }
+
+        }
+
+        System.out.println("Highest salary is "+ highest_sal);
+
+
+    }
+}
